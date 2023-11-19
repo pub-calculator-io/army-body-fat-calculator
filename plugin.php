0b15c5787b650fb4c84444784613953fdc72c275
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:29:39 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Army Body Fat Calculator by Calculator.iO
+Plugin Name: Army Body Fat Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/army-body-fat-calculator/
 Description: The Army Body Fat Calculator provides users with an accurate estimate of their body fat percentage using the standards of the U.S. Army.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Army Body Fat 
 
 function display_ci_army_body_fat_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/army-body-fat-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Army Body Fat Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_army_body_fat_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Army Body Fat Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_army_body_fat_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_army_body_fat_calculator', 'display_ci_army_body_fat_calculator' );
--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ The Army Body Fat Calculator provides users with an accurate estimate of their b
 
 ## Installation
 
-1. [Download](https://github.com/pub-calculator-io/age-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
+1. [Download](https://github.com/pub-calculator-io/army-body-fat-calculator/archive/refs/heads/master.zip) the ZIP file of this repository.
 2. Upload the /army-body-fat-calculator-master/ folder to the /wp-content/plugins/ directory.
 3. Activate the [Army Body Fat Calculator](https://www.calculator.io/army-body-fat-calculator/ "Army Body Fat Calculator Homepage") plugin through the "Plugins" menu in WordPress.
 
